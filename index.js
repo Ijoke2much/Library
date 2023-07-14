3e70a67ca7e0335a35a3e75fb12f5424062f5bc1
Ijoke2much <nilesloversoofficial@gmail.com>
2023-07-14 16:53:53 -0400

    Added boxing shadows for cards

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 // the container for the book card
 
 const formView = document.querySelector('.form-container'); // Brings up entire form.
-
 const form = document.querySelector('.formElement');
-
 const formButton = document.getElementById('open-form');
-
 const bookInfo = document.getElementById('card-wrapper'); // the info inside the book card
-
-const bookCard = document.getElementById('card-container'); // contains the div for the card content to appear in HTML.
+const removeBtn = document.createElement('div');
+const readBtn = document.createElement('div'); // Testing
 
 // NEXT SECTION
 
@@ -29,67 +26,80 @@ function Book (title, author, pages, read) {
 
 // takes the values from the form to add to the objects of the book function.
 function addBookToLibrary() {
-  event.preventDefault(); // prevents the page from refreshing.
-
+  // prevents the page from refreshing.
+  event.preventDefault(); 
   // takes the values of the inputs and displays them in the DOM
   const title = (document.getElementById('title')).value;
   const author = (document.getElementById('author')).value;
   const pages = (document.getElementById('pages')).value;
-  const read = (document.getElementById('read')).value;
+  const read = (document.getElementById('read')).checked;
 
   // values will be placed within the parameter
   newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook); // pushes arrays to the newBook array
   displayBooks(myLibrary, bookInfo);
+  formReset()
   readCheck();
-  formToggle(); // activates the on or off switch when button is pressed.
-  console.log(myLibrary) // shows the display of the array output.
+  formToggle(); 
+  // for testing
+  console.log(myLibrary);
   return false; // prevents page from submitting and refreshing.
 }
 
 // function that displays the book cards.
 // generates a div card display to view each book.
 
-function displayBooks(theLibrary, bookView) {
+function displayBooks() {
   // for each of the prameter of the arrays of "book", i will iterate through the book array."
 
   bookInfo.innerHTML = "";
-  theLibrary.forEach((book, i) => {
-    // includes the template for the card.
-    // test what happens if data-info${i} is taken out
-    const card = `<div id="card-container" data-info${i}> 
-                    <div id="card-info-wrap">
-                      <h2>Title: ${book.title}</h2>
-                      <h3>Author: ${book.author}</h3>
-                      <h3>Pages: ${book.pages}</h3>
-                      <div class="readCheck" onclick="readCheck()"><h3> Read </h3></div>
-                      <button onclick="removeBook()" class="removeBtn"> Remove </button>
-                    </div>
-                  </div>`
 
-    // creates a div
+  // TEST
+  for (let i = 0; i < myLibrary.length; i++) {
+    removeBtn.innerHTML = `<button onclick="removeBook(${i})" class="removeBtn"> Remove </button>`;
+
+    if (myLibrary[i].read) {
+      readBtn.innerHTML = `<div class="readCheck" onclick="readCheck(${i})"><h3> Read </h3></div>`
+    } else {
+      readBtn.innerHTML = `<div class="notReadCheck" onclick="readCheck(${i})"><h3> Read </h3></div>`
+    }
     const divElement = document.createElement('div');
-    // the new div element will have the content of the book card inside.
-    divElement.innerHTML = card;
-    bookView.appendChild(divElement.firstChild);
-  });
+    divElement.innerHTML =
+                `<div id="card-container">
+                  <div id="card-info-wrap">
+                    <div><h2>Title: ${myLibrary[i].title}</h2></div>
+                    <div><h3>Author: ${myLibrary[i].author}</h3></div>
+                    <div><h3>Pages: ${myLibrary[i].pages}</h3></div>
+                    ${readBtn.innerHTML}
+                    ${removeBtn.innerHTML}
+                  </div>
+                </div>`;
+    bookInfo.append(divElement);
+  };
 };
 
-// removes book card.
-// deletes everything and not indidivually.
-// function removeBook() {
-//   bookInfo.parentNode.removeChild(this);
-// }
+function removeBook(i) {
+  myLibrary.splice(i, 1);
+  displayBooks();
+}
 
 // makes read button changes on click
-function readCheck() {
-  if (read.checked) {
-    read.checked = false;
-    document.querySelector('.readCheck').style.backgroundColor = 'lightgreen';
+function readCheck(i) {
+  if (myLibrary[i].read) {
+    myLibrary[i].read = false;
+    document.querySelector('.readCheck');
   } else {
-    read.checked = true;
-    document.querySelector('.readCheck').style.backgroundColor = 'lightsalmon';
+    myLibrary[i].read = true;
+    document.querySelector('.notReadCheck');
   }
+  displayBooks();
+}
+
+// resets form values
+function formReset() {
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('pages').value = "";
 }
 
 // function makes menu visible.
